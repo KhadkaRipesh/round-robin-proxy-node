04e3d9422bfccf19b15c8f644b94a4525549c402
Ripesh Khadka <khadkanripesh@gmail.com>
2025-08-05 10:47:40 +0545

    [init]: simple app with multiple server and reverse proxy

--- proxy.js
+++ proxy.js
@@ -4,18 +4,15 @@ const httpProxy = require("http-proxy");
 const proxy = httpProxy.createProxyServer({});
 const servers = ["http://localhost:3000", "http://localhost:3001"];
 
-let i = 0;
+let serverIndex = 0;
 
-// round-robin load balancer
 const server = http.createServer((req, res) => {
-  const target = servers[i % servers.length];
-  console.log(`forwarding to : ${target}`);
-  proxy.web(req, res, {
-    target,
-  });
-  i++;
+  const target = servers[serverIndex % servers.length];
+  console.log(`forwarding to server: ${target}`);
+  proxy.web(req, res, { target });
+  serverIndex++;
 });
 
 server.listen(8000, () => {
-  console.log("Reverse Proxy running at http://localhost:8000");
+  console.log("Reverse proxy running on port 8000");
 });
